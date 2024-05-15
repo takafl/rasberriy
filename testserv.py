4bb9fb44fea823f7eb0177a98f21a3f4934c6702
Your Name <you@example.com>
2024-05-15 21:13:39 +0300

    t

--- testserv.py
+++ testserv.py
@@ -3,7 +3,7 @@ from time import sleep
 
 # إعداد محرك السيرفو على البن رقم 17 (يمكنك تغيير رقم البن حسب الحاجة)
 servo = Servo(27)
-p16=LED(17)
+p16=LED()
 p16.on()
 
 try:
@@ -11,25 +11,9 @@ try:
         # تحريك السيرفو إلى أقصى اليسار
         servo.min()
         print("Servo is at min position.")
-        sleep(1)
         
-        # تحريك السيرفو إلى المنتصف
-        servo.mid()
-        print("Servo is at mid position.")
-        sleep(1)
         
-        # تحريك السيرفو إلى أقصى اليمين
-        servo.max()
-        print("Servo is at max position.")
-        sleep(1)
-        
-        # تحريك السيرفو إلى موقع معين (نسبة مئوية)
-        positions = [-1, -0.5, 0, 0.5, 1]
-        for position in positions:
-            servo.value = position
-            print(f"Servo is at position {position}.")
-            sleep(1)
-            
+
 except KeyboardInterrupt:
     # تحرير الموارد عند انتهاء البرنامج أو عند مقاطعة المستخدم
     print("Exiting program.")
